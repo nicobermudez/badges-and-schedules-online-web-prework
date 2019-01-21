17f4db548fedd6532dec7e041240203d6b43113f
Nicolas Bermudez <bermnicolas@gmail.com>
2019-01-21 19:29:09 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -9,7 +9,6 @@ def batch_badge_creator(array)
 end
 
 def assign_rooms(array)
-  array.collect {|
   array.each_with_index.collect { |name, index| "Hello, #{name}! You'll be assigned to room #{index+ 1}!"
     
   }
